72c7c4cf6a5d5f9d0ce2b9f5b0aad3116b329ff2
Ajinkya Apte <Ajinkya_Apte@epam.com>
2022-08-12 18:20:11 +0530

    Console Apps Implementing : Azure service bus (queues/topics), Azure storage queues

---
+++ mslearn-service-bus/implement-message-workflows-with-service-bus/src/start/privatemessagereceiver/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace privatemessagereceiver
+{
+    class Program
+    {
+
+        const string ServiceBusConnectionString = "Endpoint=sb://ajinkya-learn-service-bus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=EbY/Hp1Zg+nxOd+aNfwr657435pzbCYr04y8HQ087y0=";
+        const string QueueName = "salesmessages";
+        static ServiceBusProcessor processor;
+        static void Main(string[] args)
+        {
+
+            ReceiveSalesMessageAsync().GetAwaiter().GetResult();
+
+        }
+
+        static async Task ReceiveSalesMessageAsync()
+        {
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after receiving all the messages.");
+            Console.WriteLine("======================================================");
+
+
+            var client = new ServiceBusClient(ServiceBusConnectionString);
+
+            //var processorOptions = new ServiceBusProcessorOptions
+            //{
+            //    MaxConcurrentCalls = 1,
+            //    AutoCompleteMessages = true
+            //};
+
+            //processor = client.CreateProcessor(QueueName, processorOptions);
+            processor = client.CreateProcessor(QueueName);
+
+            processor.ProcessMessageAsync += MessageHandler;
+            processor.ProcessErrorAsync += ErrorHandler;
+
+            await processor.StartProcessingAsync();
+
+            Console.Read();
+
+            await processor.CloseAsync();
+        }
+
+        // handle received messages
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+
+            // complete the message. messages is deleted from the queue. 
+            //await args.CompleteMessageAsync(args.Message);
+        }
+
+        // handle any errors when receiving messages
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
